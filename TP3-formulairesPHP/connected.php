6ee851f59ef5c1b6e414b1b406a6053fbf2b7275
Patarot <adele.patarot@gmail.com>
2023-10-11 09:55:37 +0200

    TP formulaire exo2

--- TP3-formulairesPHP/connected.php
+++ TP3-formulairesPHP/connected.php
@@ -1,26 +1,42 @@
 <?php
-// on simule une base de données
-$users = array(
-// login => password
-'adele' => 'adele',
-'guigui' => 'guigui' );
-$login = "anonymous";
-$errorText = "";
-$successfullyLogged = false;
-if(isset($_GET['login']) && isset($_GET['password'])) {
-   $tryLogin=$_GET['login'];
-   $tryPwd=$_GET['password'];
-   // si login existe et password correspond
-   if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
-      $successfullyLogged = true;
-      $login = $tryLogin;
-   } else
-      $errorText = "Erreur de login/password";
-   } else
+   // on simule une base de données
+   $users = array(
+      // login => password
+      'adele' => 'adele',
+      'guigui' => 'guigui' 
+   );
+
+   $login = "anonymous";
+   $errorText = "";
+   $successfullyLogged = false;
+   //Methode avec GET   
+   // if(isset($_GET['login']) && isset($_GET['password'])) {
+   //    $tryLogin=$_GET['login'];
+   //    $tryPwd=$_GET['password'];
+   //    // si login existe et password correspond
+   //    if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
+   //       $successfullyLogged = true;
+   //       $login = $tryLogin;
+   //    } else
+   //       $errorText = "Erreur de login/password";
+   // } 
+   
+   //Methode avec POST
+   if(isset($_POST['login']) && isset($_POST['password'])) {
+      $tryLogin=$_POST['login'];
+      $tryPwd=$_POST['password'];
+      // si login existe et password correspond
+      if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
+         $successfullyLogged = true;
+         $login = $tryLogin;
+      } else
+         $errorText = "Erreur de login/password";
+   } else 
       $errorText = "Merci d'utiliser le formulaire de login";
-   if(!$successfullyLogged) {
-      echo $errorText;
-   } else {
-      echo "<h1>Bienvenu ".$login."</h1>";
-   }
+      if(!$successfullyLogged) {
+         echo $errorText;
+      } else {
+         echo "<h1>Bienvenu ".$login."</h1>";
+      }
+   
 ?>
