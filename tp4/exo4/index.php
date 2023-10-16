b2c8df067567b6ae2bea8fdd102e00de2c1ce04c
Patarot <adele.patarot@gmail.com>
2023-10-16 17:09:42 +0200

    TP4 exo4 Add en cours

--- tp4/exo4/index.php
+++ tp4/exo4/index.php
@@ -3,6 +3,7 @@
    $sql_read= file_get_contents('sql/read.sql');
    $users=$pdo->query($sql_read)->fetchAll(PDO::FETCH_OBJ);
 ?>
+<h1>My Data Base</h1>
 <table border="1">
    <tr >
       <th>ID</th>
@@ -18,14 +19,29 @@
       </tr>";
    } ?>
 </table>
+<h2>Add new user</h2>
+<form id="add_form" action="index.php" method="post">
+   <label for="name">Name:</label>
+   <input type="text" id="name" name="name">
+   <label for="email">Email:</label>
+   <input type="text" id="email" name="email">
+   <input type="submit" value="add">
+
+</form>
 
 <?php
-   $add_request=$pdo->prepare('INSERT INTO users (name, email) VALUES (?,?)');
-   $name='antho';
-   $email='antho.bg@gmail.net';
-   $add_request->execute([$name, $email]);
+   if(isset($_POST['name']) && isset($_POST['email'])){
+      // ajouter la condition qu'il n'y ait pas deux fois le meme email
+      $add_request=$pdo->prepare('INSERT INTO users (name, email) VALUES (?,?)');
+      $name=$_POST['name'];
+      $email=$_POST['email'];
+      $add_request->execute([$name, $email]);
+      
+   }
 ?>
 <?php
    /*** close the database connection ***/
+   $_POST['name']=NULL;
+   $_POST['email']=NULL;
    $pdo = null;
 ?>
