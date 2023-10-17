0cbbf4a8cb3eed883a5acacd66759c0286f51b61
Patarot <adele.patarot@gmail.com>
2023-10-17 11:22:18 +0200

    TP4 exo4 terminé

--- tp4/exo4/index.php
+++ tp4/exo4/index.php
@@ -18,16 +18,20 @@
          <td>{$user->name}</td>
          <td>{$user->email}</td>
          <td>
-            <form action='index.php' method='post' onsubmit='location.reload()'>
+            <form action='index.php' method='post'>
                <input type='hidden' name='id' value='$user->id'></input>
                <input type='submit' name='btnName' value='delete'></input>
+            </form>
+            <form action='index.php' method='post'>
+               <input type='hidden' name='id' value='$user->id'></input>
+               <input type='submit' name='btnName' value='modify'></input>
             </form>
          </td>
       </tr>";
    } ?>
 </table>
 <h2>Add new user</h2>
-<form action="index.php" method="post" onsubmit="location.reload()">
+<form action="index.php" method="post">
    <label for="name">Name:</label>
    <input type="text" id="name" name="name">
    <label for="email">Email:</label>
@@ -52,18 +56,58 @@
       }
    }
 ?>
-<!-- traitement formulaire delete -->
+
+<!-- traitement formulaire delete-->
 <?php
    if(isset($_POST['id']) && isset($_POST['btnName'])) {
       $id=$_POST['id'];
       $action=$_POST['btnName'];
-      if($action=='delete'){
+      if($action=='delete') {
          $delete_request=$pdo->prepare('DELETE FROM users WHERE id=?');
          $delete_request->execute([$id]);
       }
    }
 ?>
+
+<?php
+   $userToModify = null;
+   if(isset($_POST['id']) && isset($_POST['btnName'])) {
+      $id=$_POST['id'];
+      $action=$_POST['btnName'];
+      
+      foreach ($users as $user) {
+         if ($user->id == $id) {
+            $userToModify = $user;
+            break; // Sortir de la boucle une fois que l'utilisateur est trouvé
+         }
+      }
+
+      if($action=='modify') {
+         //on affiche le formulaire de modification
+         echo "<h2>Modify User</h2>
+         <form action='index.php' method='post' >
+            <label for='name_modify'>Name:</label>
+            <input type='text' id='name_modify' name='name_modify' value='".($userToModify->name)."'>
+            <label for='email_modify'>Email:</label>
+            <input type='text' id='email_modify' name='email_modify' value='".($userToModify->email)."'>
+            <input type='hidden' id='id_modify' name='id_modify' value='".($userToModify->id)."'>
+            <input type='submit' value='modify'>
+         </form>";
+      }   
+   }
+   // traitement formulaire delete
+   if(isset($_POST['name_modify']) && isset($_POST['email_modify']) && isset($_POST['id_modify'])) {
+      $modify_request=$pdo->prepare("UPDATE users SET `name` = ?, `email` = ? WHERE `id`=?");
+      $name=$_POST['name_modify'];
+      $email=$_POST['email_modify'];
+      $id=$_POST['id_modify'];
+      $modify_request->execute([$name,$email,$id]);   
+   }
+   
+?>
+
 <?php
    /*** close the database connection ***/
    $pdo = null;
 ?>
+
