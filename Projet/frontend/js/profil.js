6d021632b9e16fa6b778968beb0fd3a2e47a72c4
Patarot <adele.patarot@gmail.com>
2023-10-24 17:33:05 +0200

    Projet - page profil

---
+++ Projet/frontend/js/profil.js
@@ -0,0 +1,59 @@
+let calculerBtn = document.getElementById("calculerBtn");
+let resultIMC = document.getElementById("resultIMC");
+let scoreIMC = document.getElementById("scoreIMC");
+let indicationIMC = document.getElementById("indicationIMC");
+
+
+let IMC=[
+   "Poids insuffisant",
+   "Poids Normal",
+   "Surpoids",
+   "Obesité"
+];
+
+function calculIMC(poids, taille) {
+   if(Number.isInteger(poids) && Number.isInteger(taille)){
+      // Conversion en centimètres
+      taille = taille / 100; 
+      
+      // Calcul de l'IMC
+      var imc = poids / (taille * taille);
+   
+     return imc.toFixed(1);
+   }
+}
+
+function quelleIndicationIMC(imc) {
+   if(imc<18.5) {
+      return IMC[0];
+   } else if(imc>=18.5 && imc<24.9) {
+      return IMC[1];
+   } else if(imc>=24.9 && imc<29.9) {
+      return IMC[2];
+   } else if(imc>=30){
+      return IMC[3];
+   }
+}
+calculerBtn.addEventListener('click', function(){
+   // faire le calcul de l'imc
+   let poidsText = document.getElementById('poidsInput').value;
+   let tailleText = document.getElementById('tailleInput').value;
+
+   let poids = parseInt(poidsText);
+   let taille = parseInt(tailleText);
+
+   
+   let imc=calculIMC(poids,taille);
+   let indication=quelleIndicationIMC(imc);
+
+   if(Number.isInteger(poids) && Number.isInteger(taille)){
+      scoreIMC.textContent='Votre IMC: ' + imc;
+      indicationIMC.textContent='Indication:    ' + indication;
+   } else {
+      scoreIMC.textContent='ERREUR: Veuillez entrer des informations valides'
+   }
+
+   //apparition des resultats
+   resultIMC.style.display="block";
+})
+
--- Projet/frontend/profil.php
+++ Projet/frontend/profil.php
@@ -14,73 +14,87 @@
          </h1>
       </div>
       <div class="row justify-content-center">
-         <div class="card m-4" style="width:500px; height:500px">
+         <div class="card m-4" style="width:500px; height:320px">
             <div class="card-header">
                <h3>Mes informations</h3>
             </div>
             <div class="card-body">
                <div class="row">
-                  <div class="col text-right">
+                  <div class="col-3 text-right">
                      <h5><span class="badge badge-secondary p-2">NOM</span></h5>
-                     <h5><span class="badge badge-secondary p-2">PRENOM</span></h5>
-                     <h5><span class="badge badge-secondary p-2">EMAIL</span></h5>
-                     <h5><span class="badge badge-secondary p-2">GENRE</span></h5>
-                     <h5><span class="badge badge-secondary p-2">POIDS</span></h5>
                   </div>
-                  <div class="col text-left">
-                     <h5>Patarot</h5>
-                     <h5>Adèle</h5>
-                     <h5>adeleaddele@gmail.com</h5>
-                     <h5>Femme</h5>
-                     <h5>60</h5>
+                  <div class="col-4">
+                     <p>Nom en base</p>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-3 text-right">
+                     <h5><span class="badge badge-secondary p-2">PRENOM</span></h5>
+                  </div>
+                  <div class="col-4">
+                     <p>Prenom en base</p>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-3 text-right">
+                     <h5><span class="badge badge-secondary p-2">EMAIL</span></h5>
+                  </div>
+                  <div class="col-4">
+                     <p>Email en base</p>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-3 text-right">
+                     <h5><span class="badge badge-secondary p-2">GENRE</span></h5>
+                  </div>
+                  <div class="col-4">
+                     <p>Nom en base</p>
                   </div>
                </div>
             </div>
          </div>
-         <div class="card m-4" style="width:500px; height:500px">
-            <div class="row">
-                  
-               </div>
-               <div class="col-3"></div>
+         <div class="card m-4" style="width:500px; height:320px">
+            <div class="card-header">
+               <h3>Calculer mon IMC</h3>
+            </div>
+            <div class="card-body">
+               <form action="get">
+                  <div class="form-group row">
+                     <label for="tailleInput" class="col-sm-2 col-form-label">Taille</label>
+                     <div class="col-sm-10">
+                        <input type="text" class="form-control" id="tailleInput" placeholder="Votre taille en cm">
+                     </div>
+                  </div>
+                  <div class="form-group row">
+                     <label for="poidsInput" class="col-sm-2 col-form-label">Poids</label>
+                     <div class="col-sm-10">
+                        <input type="text" class="form-control" id="poidsInput" placeholder="Votre Poids en kg">
+                     </div>
+                  </div>
+                  <div class="form-group row align-items-center text-right">
+                     <!-- <label class="col-2" for="genreInput">Genre</label>
+                     <select id="genreInput" class="form-control col-3 ml-3">
+                        <option selected></option>
+                        <option>Homme</option>
+                        <option>Femme</option>
+                     </select> -->
+                     <div class="col">
+                        <button id="calculerBtn"type="button" class="btn btn-success">Calculer</button>
+                     </div>
+                  </div>
+                  <div id="resultIMC" class="row align-items-center m-2" style="display:none">
+                     <div id="scoreIMC">
+                     </div>
+                     <div id="indicationIMC">
+                     </div>
+                  </div>
+               </form>
             </div>
          </div>
       </div>
-         <!-- <div class="card card-light" style="width: 500px">
-            
-            <div class="card-body">
-               <div class="card-title">
-                  Mes informations
-               </div>
-               <div class="row justfiy-content-center">
-                  <div class="col-3 text-right">
-                     <h4>
-                        <span class="badge badge-secondary">NOM</span>
-                     </h4>
-                     <h4>
-                        <span class="badge badge-secondary">PRENOM</span>
-                     </h4>
-                     <h4>
-                        <span class="badge badge-secondary">EMAIL</span>
-                     </h4>
-                     <h4>
-                        <span class="badge badge-secondary">GENRE</span>
-                     </h4>
-                     <h4>
-                        <span class="badge badge-secondary">POIDS</span>
-                     </h4>
-                  </div>
-                  <div class="col-6 text-left">
-                     <h4>PATAROT</h4>
-                     <h4>Adèle</h4>
-                     <h4>adele.patarot@gmail.com</h4>
-                     <h4>Femme</h4>
-                     <h4>60kg</h4>
-                  </div>
-               </div>
-            </div>
-         </div> -->
    </main>
    <?php
       require_once('templates/template_footer.php')
    ?>
+   <script src="js/profil.js"></script>
 </body>
