d7cfaa60f173464153a4e048e34f23c685bd8f27
Patarot <adele.patarot@gmail.com>
2023-11-06 22:03:55 +0100

    projet - api back aliments et profil

--- Projet/backend/aliments.php
+++ Projet/backend/aliments.php
@@ -5,21 +5,72 @@
    //On recupere la methode de la requete
    $methode=$_SERVER['REQUEST_METHOD'];
 
-   //On recupere l'uri de la requete
-   $uri = $_SERVER['REQUEST_URI'];
-
+   //Recuperation des Aliments par leur catégorie
    function getAlimentsByCategorie($pdo, $categorie) {
-      //Preparation and execution of the request
-      $sql = 'SELECT * FROM aliments WHERE categorie= ?';
+      //Preparation et Execution de la requete
+      $sql = 'SELECT * FROM aliments WHERE categorie = ? ORDER BY designation ASC';
+
       $statement = $pdo->prepare($sql);
       $sucess=$statement->execute([$categorie]);
 
-      if($sucess === false ) { //error
+      if($sucess === false ) { //Erreur
          http_response_code(500);
-         return json_encode(['error'=>'Erreur SQl']);
-      } else { //sucess
+         return json_encode(['Erreur SQL']);
+      } else { //Succès
          http_response_code(200);
          return json_encode($statement->fetchAll(PDO::FETCH_OBJ));
       }
    }
+
+   //Ajout d'un aliment
+   function ajouterAliment($pdo, $designation, $categorie, $calories, $proteine, $glucide, $lipide, $sel, $sucre) {
+      //Verification de la redondance
+      $sql_redondance = $pdo->prepare('SELECT COUNT(*) FROM aliments WHERE designation = ?');
+      $sql_redondance->execute([$designation]);
+      $exit = $sql_redondance->fetchColumn();
+      
+      //Requete d'ajout
+      $add_request=$pdo->prepare('INSERT INTO aliments (designation, categorie, calories, proteine, glucide, lipide, sel, sucre) VALUES (?,?,?,?,?,?,?,?)');
+      
+      if($exit==0) {
+         $sucess=$add_request->execute([$designation, $categorie, $calories, $proteine, $glucide, $lipide, $sel, $sucre]);
+         if($sucess === false ) {
+            http_response_code(500);
+            return json_encode(['Erreur SQL']);
+         } else {
+            http_response_code(201);
+            echo(json_encode(['Aliment ajoute avec Succes']));
+            return json_encode(['Aliment ajoute avec Succes']);
+         }
+      }
+   }
+
+   if($methode === 'GET') {
+      if (isset($_GET['categorie'])) {
+         $categorie = $_GET['categorie'];
+         // echo getAlimentsByCategorie($pdo, $categorie);
+         return getAlimentsByCategorie($pdo, $categorie);
+      } else {
+         http_response_code(500);
+         return json_encode(['Erreur Données']);
+      }
+   }
+
+   if($methode ==="POST") {      
+      if(isset($_POST['designation']) && isset($_POST['categorie']) && isset($_POST['calorie']) && isset($_POST['proteine']) && isset($_POST['glucide']) && isset($_POST['lipide']) && isset($_POST['sel'])&& isset($_POST['sucre'])) {
+         $designation = $_POST['designation'];
+         $categorie = $_POST['categorie'];
+         $calories = $_POST['calorie'];
+         $proteine = $_POST['proteine'];
+         $glucide = $_POST['glucide'];
+         $lipide = $_POST['lipide'];
+         $sel = $_POST['sel'];
+         $sucre = $_POST['sucre'];
+         return ajouterAliment($pdo, $designation, $categorie, $calories, $proteine, $glucide, $lipide, $sel, $sucre);
+      } else {
+         http_response_code(500);
+         return json_encode(['Erreur Données']);
+      }
+   }
+
 ?>
--- Projet/backend/utilisateurs.php
+++ Projet/backend/utilisateurs.php
@@ -5,79 +5,64 @@
    //On recupere la methode de la requete
    $methode=$_SERVER['REQUEST_METHOD'];
 
-   //On recupere l'uri de la requete
-   $uri = $_SERVER['REQUEST_URI'];
-
-   //definition de put
+   //Definition de PUT
    parse_str(file_get_contents("php://input"), $_PUT);
 
+   /*****************************************************FONCTIONS*************************************************/
    //Recuperation des utilisateurs
    function getUtilisateurs($pdo) {
-      //Preparation and execution of the request
+      //Preparation et Execution de la requete
       $sql = 'SELECT * FROM utilisateurs';
       $statement = $pdo->query($sql);
     
-      if ($statement === false) { //Error
+      if ($statement === false) { //Erreur
          return json_encode(['error' => 'Erreur SQL']);
-      } else {
+      } else { //Succès
          $users = $statement->fetchAll(PDO::FETCH_OBJ);
          return json_encode($users);
       }
    }
 
-   //Modifier l'utilisateur
+   //Modifier l'utilisateur par l'ID
    function modifierUtilisateur($pdo, $id, $nom, $prenom, $email, $sexe, $age, $poids, $taille, $sport) {
-      //Preparation et execution de la requete
+      //Preparation et Execution de la requete
       $sql=$pdo->prepare("UPDATE utilisateurs SET `nom` = ?, `prenom` = ?, `email` = ?, `age` = ?, `taille` = ?, `poids` = ?, `sexe` = ?, `sport` = ?  WHERE `id_utilisateur`=?");
       $sucess=$sql->execute([$nom, $prenom, $email, $age, $poids, $taille, $sexe, $sport, $id]);  
          
-      if($sucess === false ) { //error
+      if($sucess === false ) { //Erreur
          http_response_code(500);
-         // echo json_encode(['error'=>'Erreur SQl']);
-         return json_encode(['error'=>'Erreur SQl']);
-      } else { //sucess
+         return json_encode(['Erreur SQL']);
+      } else { //Succès
          http_response_code(200);
-         // echo json_encode(['User modified']);
-         return json_encode(['User modified']);
+         return json_encode(['Utilisateur modifié avec Succès']);
       }
    }
 
-   //Arbre de requetes
-   // if($methode==='GET') { 
-   //    $utilisateurs=getUtilisateurs($pdo);
-   //    // print_r($utilisateurs);
-   //    // return $utilisateurs;
-   //    // return getUtilisateurs($pdo);
-   // }
-
-   // if(isset($_POST["update"])) {
-   //    if(isset($_POST['id_utilisateur']) && isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['sexe'])&& isset($_POST['age']) && isset($_POST['poids']) && isset($_POST['taille']) && isset($_POST['activite'])) {
-   //      $result = modifierUtilisateur($pdo, $_POST['id_utilisateur'], $_POST['nom'], $_POST['prenom'], $_POST['email'], $_POST['sexe'], $_POST['age'], $_POST['poids'], $_POST['taille'], $_POST['activite']);
-   //       echo($result);
-   //    } else {
-   //       $result='erreur Formulaire';
-   //    }
-   // }
-
-
+   /*****************************************************REQUETES*************************************************/
+   //Requete de d'obtention des Utilisateurs
    if($methode === 'GET') {
       $utilisateurs = getUtilisateurs($pdo);
-      echo($utilisateurs);
+      // echo($utilisateurs);
       return $utilisateurs;
-   } else if($methode === 'PUT') {
+   }
+   
+   //Requete de Mise à Jour d'un Utilisateur
+   if($methode === 'PUT') {
       if(isset($_PUT["id_utilisateur"]) && isset($_PUT["nom"]) && isset($_PUT["prenom"]) && isset($_PUT["email"]) && isset($_PUT["sexe"]) && isset($_PUT["age"]) && isset($_PUT["poids"]) && isset($_PUT["taille"]) && isset($_PUT["activite"])){
-         $id=$_PUT["id_utilisateur"];
-         $nom=$_PUT["nom"];
-         $prenom=$_PUT["prenom"];
-         $email=$_PUT["email"];
-         $sexe=$_PUT["sexe"];
-         $age=$_PUT["age"];
-         $poids=$_PUT["poids"];
-         $taille=$_PUT["taille"];
-         $sport=$_PUT["activite"];
-         
+         $id = $_PUT["id_utilisateur"];
+         $nom = $_PUT["nom"];
+         $prenom = $_PUT["prenom"];
+         $email = $_PUT["email"];
+         $sexe = $_PUT["sexe"];
+         $age = $_PUT["age"];
+         $poids = $_PUT["poids"];
+         $taille = $_PUT["taille"];
+         $sport = $_PUT["activite"];
          $resultat = modifierUtilisateur($pdo, $id, $nom, $prenom, $email, $sexe, $age, $poids, $taille, $sport);
-         echo($resultat);
-         return $resultat;
+      } else {
+         http_response_code(500);
+         $resultat = json_encode(['Erreur Données']);
       }
+      // echo($resultat);
+      return $resultat;
    }
